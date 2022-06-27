7a97e93cb33d623af2a31fe807a4fd2cbc98cb6d
Ivan <gubarenkovanek@gmail.com>
2022-06-27 22:22:01 +0300

    edits
    all edits in location and fix franshize

--- src/js/location.js
+++ src/js/location.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-location-open]"),
+    closeModalBtn: document.querySelector("[data-location-close]"),
+    modal: document.querySelector("[data-location]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
