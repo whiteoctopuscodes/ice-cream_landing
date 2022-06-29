323a497b48b7ebd3c97a8d9322857ac2943c4c98
whiteoctopuscodes <103589181+whiteoctopuscodes@users.noreply.github.com>
2022-06-29 13:38:29 +0000

    Deploying to gh-pages from @ whiteoctopuscodes/ice-cream_landing@3d30f8449bc41cfee6c3b4a1f3ed4aad2094de19 🚀

---
+++ index.846ae373.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".btn-home"),o=document.querySelector("#home-hero");e.addEventListener("click",(()=>{var e;e=o,window.scroll({left:0,top:e.offsetTop,behavior:"smooth"})}));
+//# sourceMappingURL=index.846ae373.js.map
---
+++ index.9b595f68.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".btn-home"),o=document.querySelector("#home-hero");e.addEventListener("click",(function(){var e;e=o,window.scroll({left:0,top:e.offsetTop,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.9b595f68.js.map
