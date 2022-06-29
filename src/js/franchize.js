7924ba5076bf8382fd1cebbb19c71fcc6baaacd7
Ivan <gubarenkovanek@gmail.com>
2022-06-29 18:46:41 +0300

    edits

--- src/js/franchize.js
+++ src/js/franchize.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    openModalBtn: document.querySelector("[data-franchize-open]"),
+    closeModalBtn: document.querySelector("[data-franchize-close]"),
+    modal: document.querySelector("[data-franchize]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
