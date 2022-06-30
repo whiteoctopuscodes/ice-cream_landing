6d768633110dc64f1feee24e1ae71de5c882d5d2
margobelikova <margo.belikova@gmail.com>
2022-06-30 15:19:09 +0300

    добавили модалки

---
+++ src/js/franchize.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-franchize-open]"),
+        closeModalBtn: document.querySelector("[data-franchize-close]"),
+        modal: document.querySelector("[data-franchize]"),
+      };
+    
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
+    
+      function toggleModal() {
+        refs.modal.classList.toggle("is_hidden");
+      }
+    })(); 
